0aa8c7c741481d837d20e5f58bf7a96daef00138
Laurent Bovet <laurent.bovet@windmaster.ch>
2018-10-29 11:50:22 +0100

    Add graceful shutdown

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const app = require('express')()
 const moment = require('moment')
+const GracefulShutdownManager = require('@moebius/http-graceful-shutdown').GracefulShutdownManager;
+
 const port = process.env.SERVICE_PORT
 
 var counter = 0;
@@ -8,16 +10,25 @@ var counter = 0;
 app.get('/counter', (req, res) => {    
     counter++;
     log(`Counting: ${counter}`);
-    res.send({counter});
+    setTimeout( ()=> res.send({counter}), 5000);
 });
 
 app.get('/health/ready', (req, res) => res.send());
 app.get('/health/live', (req, res) => res.send());
 
-app.listen(port, () => log(`Listening on port ${port}`));
+const server = app.listen(port, () => log(`Listening on port ${port}`));
+const shutdownManager = new GracefulShutdownManager(server);
+
+process.on('SIGTERM', () => {
+    shutdownManager.terminate(() => {
+        log('Server gracefully terminated');
+    });
+});
 
 function log(message) {
     Object.assign(this, process.env)
     var timestamp = moment().format("YYYY-MM-DD hh:mm:ss,SSS");
     console.log(`${timestamp} ${SYSTEM_INSTANCE} contador ${SYSTEM_ENV} ${SERVICE_INSTANCE} INFO ${message}`);
 }
+
+log(`Process started with PID ${process.pid}`);
